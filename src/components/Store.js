567c1dc4dd443da0926118062010ad5b7851f38d
CH <1925480119@qq.com>
2020-10-07 00:46:03 +0800

    [chen] submitPhone

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { ShoppingCartOutlined } from '@ant-design/icons';
+import './phone.scss';
 
 class Store extends React.Component {
   render() {
-    return <div className="header">Store</div>;
+    return (
+      <div>
+        <div className="header">
+          <span>Store</span>
+          <div className="icons-list">
+            <ShoppingCartOutlined />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 export default Store;
